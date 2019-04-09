a3ddf4f9df1fc0fd2ad02c10aa2ebb5006048333
unknown <mohamed2m2018@gmail.com>
2019-04-09 11:16:25 +0200

    Reading the image and some calculations

--- Ass3.py
+++ Ass3.py
@@ -6,12 +6,28 @@ from scipy import signal
 # Calculating gradient at X direction
 
 
-def Ix(grey_image):
+def calculateIx(grey_image):
     kernel = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
     return signal.convolve2d(grey_image,kernel,mode='same')
 
+
+# Calculating gradient at Y direction
+
  
-def Iy(grey_image):
+def CaculateIy(grey_image):
     #transpose of the Ix kernel 
     kernel = np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]])
     return signal.convolve2d(grey_image,kernel,mode='same')   
+
+
+#Reading The image
+image = cv2.imread("box.jpg", 1)
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) 
+     
+
+#Calculating Ix,Iy,Ixx,Iyy,Ixy
+Ix=calculateIx(gray_image)
+Iy=CaculateIy(gray_image)
+Ixx = pow(Ix,2)
+Iyy = pow(Iy,2)
+Ixy = Ix*Iy
